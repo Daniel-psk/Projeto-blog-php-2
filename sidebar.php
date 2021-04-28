6d74b86892e96a40f354e603a0628cdf33d48dbb
Daniel-psk <83319334+Daniel-psk@users.noreply.github.com>
2021-04-28 17:49:38 -0300

    Adicionado template personalizado

---
+++ sidebar.php
@@ -0,0 +1,27 @@
+<div id="sidebar">
+	<div class="inner">
+
+		<!-- Search -->
+            <?php
+                require_once("search.php")
+            ?>
+	    <!-- Menu -->
+			<?php
+                require_once("menu.php")
+            ?>
+
+	    <!-- Section -->
+            <?php
+                require("section3.php")
+            ?>
+                                
+		<!-- Section -->
+            <?php
+                require("section4.php")
+            ?>
+		<!-- Footer -->
+            <?php
+                require_once("footer.php")
+            ?>
+	</div>
+</div>
